dcc42261330ae2c5fa0d91e38e3fc4a706bf57ca
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-08-03 00:28:51 +0300

    implement save.h

--- include/save.h
+++ include/save.h
@@ -1,6 +1,15 @@
 #ifndef CHANGE_STYLE_OF_NAMING_SAVE_H
 #define CHANGE_STYLE_OF_NAMING_SAVE_H
 
+#include "project.h"
 
+class Save {
+ public:
+  Save(const Project& project);
+ private:
+  const Project& project_;
+  void MakeFile();
+  void ChangeFile();
+};
 
 #endif //CHANGE_STYLE_OF_NAMING_SAVE_H
--- src/save.cpp
+++ src/save.cpp
@@ -1,1 +1,14 @@
 #include "save.h"
+Save::Save(const Project& project) :project_(project) {
+  if (project_.GetWorkWFile() == create_new) {
+    MakeFile();
+  } else {
+    ChangeFile();
+  }
+}
+void Save::MakeFile() {
+
+}
+void Save::ChangeFile() {
+
+}
